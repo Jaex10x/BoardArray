b135633c437d8a66fdb07960dbd3cda5668ec383
johnlloyd <johnlloydencierto@gmail.com>
2025-09-21 23:36:33 +0800

    new changes AHAHAHH

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,41 +14,31 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            //makabuang sir
+            int pos = 0;
 
-            if(index == SIZE){
-                if(!entry ->compare(&array[SIZE - 1])){
-                    cout << entry -> name << "'s score is too low to be added!" << endl;
-                    return;
-                }
+            while (pos < index && !entry->compare(&array[pos])) {
+                pos++;
             }
 
-            int pos = index;
-
-            for(int i = 0 ; i < index ; i++){
-                if(entry->compare(&array[i])){
-                    pos = i;
-                    break;
+            if (index < SIZE) {
+                for (int i = index; i > pos; i--) {
+                    array[i] = array[i - 1];
+                }
+                array[pos] = *entry;
+                index++;
+            }
+            else {
+                if (entry->compare(&array[SIZE - 1])) {
+                    for (int i = SIZE - 1; i > pos; i--) {
+                        array[i] = array[i - 1];
+                    }
+                    array[pos] = *entry;
+                } else {
+                    cout << entry->name << "'s score is too low to be added!" << endl;
                 }
             }
-
-            int limit;
-                if(index < SIZE){
-                limit = index;
-                }else{
-                limit = SIZE - 1;
-                }
-            for(int j = index ; j > pos ;j--){
-                array[j] = array[j - 1];
-            }
-
-            array[pos] = *entry;
-            if(index < SIZE) index++;
-            return;
-           
         }
-
         void print() {
             for (int i = 0; i < index ; i++) {
                 cout << i + 1 << ". ";
